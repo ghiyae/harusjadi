1e35d11d3edcbab928f2a902a3c0863f4b2bbf3a
ghiyae <gia.frediawan@gmail.com>
2020-03-04 20:54:27 +0700

    Update tes.php

--- application/controllers/tes.php
+++ application/controllers/tes.php
@@ -1,1 +1,14 @@
-tes.php
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tes extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file tes.php */
+/* Location: ./application/controllers/tes.php */
