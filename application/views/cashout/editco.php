2878be304d0c5a04be387cf7a305d323117468db
ghiyae <gia.frediawan@gmail.com>
2020-03-08 12:20:16 +0700

    Update editco.php

--- application/views/cashout/editco.php
+++ application/views/cashout/editco.php
@@ -1,55 +1,51 @@
-              <!-- Basic Card Example -->
-              <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Basic Card Example</h6>
-                </div>
-                <div class="card-body">
 
-                  <form>
-                    <div class="form-row">
-                      <div class="form-group col-md-6">
-                        <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4">
-                      </div>
-                      <div class="form-group col-md-6">
-                        <label for="inputPassword4">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label for="inputAddress">Address</label>
-                      <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                    </div>
-                    <div class="form-group">
-                      <label for="inputAddress2">Address 2</label>
-                      <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                    </div>
-                    <div class="form-row">
-                      <div class="form-group col-md-6">
-                        <label for="inputCity">City</label>
-                        <input type="text" class="form-control" id="inputCity">
-                      </div>
-                      <div class="form-group col-md-4">
-                        <label for="inputState">State</label>
-                        <select id="inputState" class="form-control">
-                          <option selected>Choose...</option>
-                          <option>...</option>
-                        </select>
-                      </div>
-                      <div class="form-group col-md-2">
-                        <label for="inputZip">Zip</label>
-                        <input type="text" class="form-control" id="inputZip">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
-                          Check me out
-                        </label>
-                      </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Sign in</button>
-                  </form>
-                </div>
-              </div>
+<!-- Basic Card Example -->
+<div class="card shadow mb-4">
+  <div class="card-header py-3">
+    <h6 class="m-0 font-weight-bold text-primary">Form Input Cash Out</h6>
+  </div>
+  <div class="card-body">
+
+    <form method="post" action="">
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="description">Description</label>
+          <textarea type="text" class="form-control <?php echo form_error('description')?'is-invalid':'' ?>" id="description" name="description"></textarea>
+          <div class="invalid-feedback">
+            <?php echo form_error('description') ?>
+          </div>
+        </div>
+
+        <div class="form-group col-md-3">
+          <label for="category">Category</label>
+          <select id="category" name="category" class="form-control <?php echo form_error('category')?'is-invalid':'' ?>">
+            <option selected hidden value="">Pilih Category...</option>
+            <?php foreach ($category as $dt) : ?>
+              <option value ="<?= $dt->category_id ?>" <?=set_value('category')=="$dt->category_id"?"selected":'' ?>> <?=$dt->category;?></option>
+            <?php endforeach ?>
+          </select>
+          <div class="invalid-feedback">
+            <?php echo form_error('category') ?>
+          </div>
+        </div>
+
+        <div class="form-group col-md-3">
+          <label for="jumlah">Jumlah</label>
+          <input type="text" class="form-control <?php echo form_error('jumlah')?'is-invalid':'' ?>" id="jumlah" name="jumlah">
+          <div class="invalid-feedback">
+            <?php echo form_error('jumlah') ?>
+          </div>
+        </div>
+        <div class="form-group col-md-3">
+          <label for="tanggal">Tanggal</label>
+          <input type="date" class="form-control <?php echo form_error('tanggal')?'is-invalid':'' ?>" id="tanggal" name="tanggal">
+          <div class="invalid-feedback">
+            <?php echo form_error('tanggal') ?>
+          </div>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Update</button>
+      <a href="<?php echo base_url('cashout')?>" class="btn btn-info">Back</a>
+    </form>
+  </div>
+</div>
