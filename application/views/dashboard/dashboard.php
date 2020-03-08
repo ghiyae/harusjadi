f8f186ecee9dd9d3e9ec312ca98d5f79d98ad845
ghiyae <gia.frediawan@gmail.com>
2020-03-08 10:46:39 +0700

    Update dashboard.php

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -71,7 +71,7 @@
             <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
           </div>
           <div class="col-auto">
-            <i class="fas fa-comments fa-2x text-gray-300"></i>
+            <i class="fas fa-receipt fa-2x text-gray-300"></i>
           </div>
         </div>
       </div>
