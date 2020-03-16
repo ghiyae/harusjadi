c1972bf3a272d669fa3eaac485d3870f1a41855f
ghiyae <gia.frediawan@gmail.com>
2020-03-16 19:21:21 +0700

    Update pch.sql

--- pch.sql
+++ pch.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 28 Feb 2020 pada 14.28
--- Versi Server: 5.6.21
+-- Generation Time: Mar 16, 2020 at 01:18 PM
+-- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `cashin`
+-- Table structure for table `cashin`
 --
 
 CREATE TABLE IF NOT EXISTS `cashin` (
@@ -34,10 +34,18 @@ CREATE TABLE IF NOT EXISTS `cashin` (
   `tanggal_ci` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `cashin`
+--
+
+INSERT INTO `cashin` (`cashin_id`, `member_id`, `description`, `jumlah_ci`, `tanggal_ci`) VALUES
+('5e5fb8ade2510', '5e60aa67ded9b', 'kas', 30000, '2020-03-04'),
+('5e66376f8bb78', '5e5fabe5079be', 'tes kas', 100000, '2020-03-03');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `cashout`
+-- Table structure for table `cashout`
 --
 
 CREATE TABLE IF NOT EXISTS `cashout` (
@@ -48,10 +56,18 @@ CREATE TABLE IF NOT EXISTS `cashout` (
   `tanggal_co` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `cashout`
+--
+
+INSERT INTO `cashout` (`cashout_id`, `category_id`, `description`, `jumlah_co`, `tanggal_co`) VALUES
+('1', '5e57d6e434ed8', 'kondangan', 200000, '2020-03-08'),
+('5e6479102c940', '5e57d233e6950', 'sukarela', 100000, '2020-03-09');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE IF NOT EXISTS `category` (
@@ -60,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `category`
+-- Dumping data for table `category`
 --
 
 INSERT INTO `category` (`category_id`, `category`) VALUES
@@ -70,7 +86,7 @@ INSERT INTO `category` (`category_id`, `category`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `login`
+-- Table structure for table `login`
 --
 
 CREATE TABLE IF NOT EXISTS `login` (
@@ -81,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `login` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `login`
+-- Dumping data for table `login`
 --
 
 INSERT INTO `login` (`iduser`, `username`, `password`, `level`) VALUES
@@ -90,7 +106,7 @@ INSERT INTO `login` (`iduser`, `username`, `password`, `level`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `member`
+-- Table structure for table `member`
 --
 
 CREATE TABLE IF NOT EXISTS `member` (
@@ -102,13 +118,28 @@ CREATE TABLE IF NOT EXISTS `member` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `member`
+-- Dumping data for table `member`
 --
 
 INSERT INTO `member` (`member_id`, `nama`, `jabatan`, `golongan`, `jumlah`) VALUES
-('5e50de0b95772', 'Lusi', 'Manager', '1', 200000),
-('5e5287250f1c7', 'Indra', 'Manager', '8', 150000),
-('5e57ea6fe85f5', 'Gia Eka Freiawan', 'Staff', '2', 30000);
+('5e5bd3011575e', 'Desi Lusinda', 'Staff', '2', 50000),
+('5e5fabe5079be', 'Agus Solihin', 'Bos', '9', 100000),
+('5e60aa67ded9b', 'Nabhila Rufaidah Syahnaz', 'Staff', '3', 30000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recap`
+--
+
+CREATE TABLE IF NOT EXISTS `recap` (
+  `tanggal` date NOT NULL,
+  `description` varchar(225) NOT NULL,
+  `in` int(10) NOT NULL,
+  `out` int(10) NOT NULL,
+  `saldo` int(10) NOT NULL,
+  `id_recap` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -145,6 +176,12 @@ ALTER TABLE `member`
  ADD PRIMARY KEY (`member_id`);
 
 --
+-- Indexes for table `recap`
+--
+ALTER TABLE `recap`
+ ADD PRIMARY KEY (`id_recap`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -154,17 +191,17 @@ ALTER TABLE `member`
 ALTER TABLE `login`
 MODIFY `iduser` int(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `cashin`
+-- Constraints for table `cashin`
 --
 ALTER TABLE `cashin`
 ADD CONSTRAINT `cashin_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `cashout`
+-- Constraints for table `cashout`
 --
 ALTER TABLE `cashout`
 ADD CONSTRAINT `cashout_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE;
