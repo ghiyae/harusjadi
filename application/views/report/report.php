b74bcca924339f269a4e9b4fcc4dbce335351b02
ghiyae <gia.frediawan@gmail.com>
2020-03-31 20:55:12 +0700

    Update report.php

--- application/views/report/report.php
+++ application/views/report/report.php
@@ -1,13 +1,13 @@
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-  <h1 class="h3 mb-0 text-gray-800">Report</h1>
+  <!-- <h1 class="h3 mb-0 text-gray-800">Report</h1> -->
 </div>
 
 
               <!-- Area Chart -->
               <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Area Chart</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Report</h6>
                 </div>
                 <div class="card-body">
                   <div class="chart-area">
